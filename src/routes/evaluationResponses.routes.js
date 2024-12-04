47199774b63d7a03cf6d4203b1d3123a086904a6
Alexandretta <alxsalazar21@gmail.com>
2024-12-04 15:29:21 -0500

    se borran modulos no usados

--- src/routes/evaluationResponses.routes.js
+++
@@ -1,8 +0,0 @@
-// const express = require("express");
-// const router = express.Router();
-// const { saveEvaluationResponses, getEvaluationResponses } = require('../controllers/evaluationResponses.controller');
-
-// router.post('/evaluationresponses', saveEvaluationResponses);
-// router.get('/evaluationresponses', getEvaluationResponses);
-
-// module.exports = router;
--- src/services/evaluationService.js
+++ src/services/evaluationService.js
@@ -1,41 +1,54 @@
-const CompleteEvaluation = require('../models/CompleteEvaluation');
-const EvaluationToken = require('../models/EvaluationToken');
+const CompleteEvaluation = require("../models/CompleteEvaluation");
+const EvaluationToken = require("../models/EvaluationToken");
 
-const saveCompleteEvaluation = async (evaluationtoken, evaluationId, responses) => {
+const saveCompleteEvaluation = async (
+  evaluationtoken,
+  evaluationId,
+  responses
+) => {
   try {
-    const evaluationToken = await EvaluationToken.findOne({ evaluationToken: evaluationtoken });
+    const evaluationToken = await EvaluationToken.findOne({
+      evaluationToken: evaluationtoken,
+    });
 
     if (!evaluationToken) {
-      throw new Error('Token not found');
-    };
+      throw new Error("Token not found");
+    }
 
     if (evaluationToken.usageCount >= 2) {
-      throw new Error('Token has been used the maximum number of times allowed');
-    };
+      throw new Error(
+        "Token has been used the maximum number of times allowed"
+      );
+    }
 
-    const completeEvaluation = await CompleteEvaluation.findOne({ evaluationtoken, evaluationId });
-
-    if (!completeEvaluation) {
-     const newEvaluation = new CompleteEvaluation({
+    const completeEvaluation = await CompleteEvaluation.findOne({
       evaluationtoken,
       evaluationId,
-      responses,
     });
-    await newEvaluation.save();
-    evaluationToken.usageCount += 1;
-    await evaluationToken.save();
-    return newEvaluation;
-  } else if (evaluationToken.usageCount === 1) {
-    completeEvaluation.responses2 = responses;
-    await completeEvaluation.save();
-    evaluationToken.usageCount += 1;
-    await evaluationToken.save();
-    return completeEvaluation;
-  } else {
-    throw new Error("Token has been used the maximum number of times allowed");
-  }
+
+    if (!completeEvaluation) {
+      const newEvaluation = new CompleteEvaluation({
+        evaluationtoken,
+        evaluationId,
+        responses,
+      });
+      await newEvaluation.save();
+      evaluationToken.usageCount += 1;
+      await evaluationToken.save();
+      return newEvaluation;
+    } else if (evaluationToken.usageCount === 1) {
+      completeEvaluation.responses2 = responses;
+      await completeEvaluation.save();
+      evaluationToken.usageCount += 1;
+      await evaluationToken.save();
+      return completeEvaluation;
+    } else {
+      throw new Error(
+        "Token has been used the maximum number of times allowed"
+      );
+    }
   } catch (error) {
-    console.error('Error saving complete evaluation:', error);
+    console.error("Error saving complete evaluation:", error);
     throw error;
   }
 };
