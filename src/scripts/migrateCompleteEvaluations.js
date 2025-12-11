632117502844fe96ed4905c8eca93094bf0a3830
Alexandretta <alxsalazar21@gmail.com>
2025-12-11 12:14:03 -0500

    se corrige la creacion de tokens

--- src/scripts/migrateCompleteEvaluations.js
+++ src/scripts/migrateCompleteEvaluations.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 async function migrateCompleteEvaluations() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.DB_URI);
 
     const db = mongoose.connection.db;
     const collection = db.collection("completevaluations");
